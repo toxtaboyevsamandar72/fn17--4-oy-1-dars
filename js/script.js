464c2469af31d19c637cb613967cc648176924d2
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-01-12 00:03:03 +0500

    index html script js

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// 1-masala
+
+// let length=+prompt("uzunlik");
+// let breadth=+prompt("kenglik");
+// let height=+prompt("balandlik");
+
+// let result = length*breadth*height;
+
+// console.log(result);
+
+
+
+// 2-masala
+
+// let Number=+prompt(" soni kiriting");
+
+// let result = Number**3;
+
+// console.log(result);
+
+
+
+// 3-masala
+
+// let firstNumber=+prompt("1  soni kiriting");
+// let secondNumber=+prompt("2 soni kiriting");
+
+//  let result = (firstNumber+secondNumber)**2
+
+// console.log(result);
+
+
+
+
+
+// 3-masala
+
+
+// let firstNumber=+prompt("soni kiriting");
+
+// let result = firstNumber%10;
+
+// console.log(result);
+
+
+
+
+// 4-masala
+
+// let firstNumber=+prompt("2 honali soni kiriting");
+
+// let result =firstNumber%10+firstNumber/10%10;
+
+// console.log(result);
+
+
+
+
+
+// 5-masala
+
+// let firstNumber=+prompt("3 honali son kiriting");
+
+// let result = (firstNumber/10)%10;
+
+// console.log(result);
